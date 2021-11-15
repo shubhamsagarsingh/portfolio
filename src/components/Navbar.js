0c3e26d9b0e6ea5767d14889d5170008077da174
shubhamsagarsingh <shubham.sagar.singh@gmail.com>
2021-11-16 02:25:40 +0530

    Apply basic setup to start building the portfolio
    Created a fresh create-react-app and refactored the code by removing the un-necessary files and code. Then installed libraries like "react-router-dom@5" and "node-saas". Then created different folders inside "src" for seperating code for "components", "pages" and "scss". Then added some globally and locally used styling in the "scss" folder

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom"
+
+
+
+
+const Navbar = () => {
+    return(
+        <div className='b-navbar o-flex o-gap4'>            
+            <div className='l-navbar__left'>
+                              
+            </div>
+
+            <div className='l-navbar__right o-flex-hsa o-gap1'>
+                <ul className='e-navbar__list o-flex-hsa o-gap1 o-ul--reset'>
+                    <NavLink className='o-link--reset' to='/'><li>HOME</li></NavLink>
+                    <NavLink className='o-link--reset' to='/projects'><li>PROJECTS</li></NavLink>
+                    <NavLink className='o-link--reset' to='/about'><li>ABOUT</li></NavLink>
+                    <NavLink className='o-link--reset' to='/contact'><li>CONTACT</li></NavLink>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+
+
+
+export default Navbar
