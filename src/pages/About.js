f3226bd403c79d6ea6cef5ed26fd1e65c799c46d
shubhamsagarsingh <shubham.sagar.singh@gmail.com>
2021-11-21 09:52:16 +0530

    Create the UI of the About Page
    Created the UI of About Page.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,29 @@
 const About = () => {
     return(
         <div className='b-about o-flex-center'>
-            <div>
-                <h1>About Page</h1>                
+            <div className='e-about__card o-flex-column o-gap3'>
+                <h1>About <i>Shubham Sagar Singh</i></h1>
+                <p>I am a fullstack web developer with 2+ years of freelancing experience.</p>
+                <p>I have always been fascinated by web technology and that's why I love creating websites that are functional, responsive, asthetically pleasing and have great user experience.</p>
+                <h2>TECH STACK</h2>
+                <div className='o-flex-hsb'>
+                    <ul className='e-about__ul o-flex-column o-gap1 o-ul--reset'>
+                        <h3>FRONTEND</h3>
+                        <li>ReactJS</li>
+                        <li>Javascript(ES6+)</li>
+                        <li>SCSS</li>
+                        <li>Redux</li>
+                        <li>Axios</li>
+                        <li>Formik, Yup</li>
+                    </ul>
+                    
+                    <ul className='e-about__ul o-flex-column o-gap1 o-ul--reset'>
+                        <h3>BACKEND</h3>
+                        <li>NodeJS</li>
+                        <li>ExpressJS</li>
+                        <li>MongoDB</li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -25,37 +25,37 @@ const Contact = () => {
 
     return(
         <Formik initialValues={initialValues} validationSchema={contactSchema}>
-        <div className='b-contact o-flex-center'>
-            <div className='e-contact__card o-flex-column o-gap1'>
-                <h1>Contact Me</h1>
-                <p>If you'd like to get in touch, drop me a message and I'll get back to you within 24-48 hours.</p>
-                <Form className='e-contact__form o-flex-column o-gap1' onSubmit={submitFunc}>
-                    <div className='o-flex o-gap1'>
-                        <section className='o-flex-column o-gap1 '>
-                            <label>NAME</label>
-                            <Field type='text' name='name'/>
-                            <ErrorMessage name='name' component={ErrorStyle}/>
-                        </section>
+            <div className='b-contact o-flex-center'>
+                <div className='e-contact__card o-flex-column o-gap1'>
+                    <h1>Contact Me</h1>
+                    <p>If you'd like to get in touch, drop me a message and I'll get back to you within 24-48 hours.</p>
+                    <Form className='e-contact__form o-flex-column o-gap1' onSubmit={submitFunc}>
+                        <div className='o-flex o-gap1'>
+                            <section className='o-flex-column o-gap1 '>
+                                <label>NAME</label>
+                                <Field type='text' name='name'/>
+                                <ErrorMessage name='name' component={ErrorStyle}/>
+                            </section>
 
-                        <section className='o-flex-column o-gap1 '>
-                            <label>EMAIL</label>
-                            <Field type='email' name='email'/>
-                            <ErrorMessage name='email' component={ErrorStyle}/>
-                        </section>
-                    </div>
+                            <section className='o-flex-column o-gap1 '>
+                                <label>EMAIL</label>
+                                <Field type='email' name='email'/>
+                                <ErrorMessage name='email' component={ErrorStyle}/>
+                            </section>
+                        </div>
 
-                    <label>SUBJECT</label>
-                    <Field type='text' name='subject'/>
-                    <ErrorMessage name='subject' component={ErrorStyle}/>
+                        <label>SUBJECT</label>
+                        <Field type='text' name='subject'/>
+                        <ErrorMessage name='subject' component={ErrorStyle}/>
 
-                    <label >MESSAGE</label>
-                    <Field as='textarea' name='message'/>
-                    <ErrorMessage name='message' component={ErrorStyle}/>
+                        <label >MESSAGE</label>
+                        <Field as='textarea' name='message'/>
+                        <ErrorMessage name='message' component={ErrorStyle}/>
 
-                    <button className='e-contact__button' type='submit'>Send Message</button>
-                </Form>
+                        <button className='e-contact__button' type='submit'>Send Message</button>
+                    </Form>
+                </div>
             </div>
-        </div>
         </Formik>
     )
 }
