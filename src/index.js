3b4569be85a1ae71efec843775da8bb2a9de4d40
shubhamsagarsingh <shubham.sagar.singh@gmail.com>
2022-02-27 01:29:50 +0530

    add "basename" in BrowserRouter in index.js file - to remove BLACK PAGE while using github pages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.scss'
 
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
